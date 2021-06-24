e364c8186bc5a50da20a26069664c9d76d76b7b9
alumah00 <chrishamula@gmail.com>
2021-06-24 12:13:56 -0400

    Just pushing up the latest iteration.  Still busted

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,18 +9,87 @@ namespace ConsoleUI
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-
-
+            Console.WriteLine("Welcome to the Party!!");
+            GetUserInfo();
+            //MultiLineAnimation();
+            PrintGuestsName();
+            PrintWinner();
+            Console.ReadLine();
         }
+            private static Dictionary<int, string> guests = new Dictionary<int, string>();
+            private static int min = 1000;
+            private static int max = 9999;
+            private static int raffleNumber;
+            private static Random _rdm = new Random();
+            public static string GetUserInput(string message)
+            {
+                string output = "";
+                Console.Write(message);
+                output = Console.ReadLine();
+                return output;
+            }
 
-        //Start writing your code here
+            public static void GetUserInfo()
+            {
+                string otherGuest = "";
+                do
+                {
+                    string name = GetUserInput("Please enter your name ");
+                    raffleNumber = GenerateRandomNumber(min, max);
+                    while (String.IsNullOrEmpty(name))
+                    {
+                        name = GetUserInput("Please enter your name ");
+                    }
+                    while (guests.ContainsKey(raffleNumber))
+                    {
+                        raffleNumber = GenerateRandomNumber(min, max);
+                    }
+                    AddGuestsInRaffle(raffleNumber, name);
 
+                    otherGuest = GetUserInput("Do you want to add another name? ").ToLower();
+                    while (String.IsNullOrEmpty(otherGuest))
+                    {
+                        otherGuest = GetUserInput("Do you want to add another name? ").ToLower();
+                    }
+                } while (otherGuest == "yes" || String.IsNullOrEmpty(otherGuest));
+            }
 
+            public static int GenerateRandomNumber(int min, int max)
+            {
+                int output = _rdm.Next(min, max);
+                return output;
+            }
 
+            public static void AddGuestsInRaffle(int raffleNumber, string guest)
+            {
+                guests.Add(raffleNumber, guest);
+            }
 
+            public static void PrintGuestsName()
+            {
+                foreach (var name in guests)
+                {
+                    Console.WriteLine($"Thank you for coming {name.Value}, your raffle number is {name.Key}");
+                }
+            }
 
+            public static int GetRaffleNumber(Dictionary<int, string> people)
+            {
+                List<int> raffleNumber = people.Keys.ToList();
+                int i = _rdm.Next(raffleNumber.Count);
+                int winnerNumber = raffleNumber[i];
+                return winnerNumber;
+            }
+
+            public static void PrintWinner()
+            {
+                int winnerNumber = GetRaffleNumber(guests);
+                string winnerName = guests[GetRaffleNumber(guests)];
+                Console.WriteLine($"The Winner is: {winnerName} with the #{winnerNumber}!!");
+            }
 
         static void MultiLineAnimation() // Credit: https://www.michalbialecki.com/2018/05/25/how-to-make-you-console-app-look-cool/
         {
@@ -70,4 +139,8 @@ namespace ConsoleUI
             }
         }
     }
-}
+        
+        //Start writing your code here
+
+        
+    }
